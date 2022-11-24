a3bba027899ad71f4daf3b8b2e49b5031a29a80f
synth Eater <90665040+SynthEater@users.noreply.github.com>
2022-11-23 19:53:24 -0500

    encryption working
    outputs enc data but as array, needs fixing

---
+++ app.js
@@ -0,0 +1,28 @@
+function main() {
+
+    //ENCRYPTION
+
+    //generate key
+    let key = MDE.Keygen.random(8);
+
+    //encrypt data
+    let dom_text = document.querySelector('.input');
+    let enc = MDE.Encrypt(dom_text.textContent, key);
+
+    //output encrypted data
+    let dom_output = document.querySelector('.output');
+
+    let dataToChar = [];
+    for (let i = 0; i < enc.data.length; i++) {
+        dataToChar.push(String.fromCharCode(enc.data[i] % 127));
+    }
+
+    dom_output.textContent = dataToChar;
+
+
+
+
+    //DECRIPTION
+    console.log(enc);
+
+}
