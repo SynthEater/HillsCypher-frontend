2d71c58db290f8284a4ca47e82be23772ad9530a
synth Eater <90665040+SynthEater@users.noreply.github.com>
2022-11-23 23:29:40 -0500

    working on decrypt

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-function main() {
-
+function encrypt() {
     //ENCRYPTION
 
     //generate key
     let key = MDE.Keygen.random(8);
+    let dom_keyOutput = document.querySelector('.key');
+    dom_keyOutput.textContent = key;
 
     //encrypt data
     let dom_text = document.querySelector('.input');
@@ -11,18 +12,18 @@ function main() {
 
     //output encrypted data
     let dom_output = document.querySelector('.output');
-
     let dataToChar = [];
     for (let i = 0; i < enc.data.length; i++) {
         dataToChar.push(String.fromCharCode(enc.data[i] % 127));
     }
-
-    dom_output.textContent = dataToChar;
-
+    dom_output.textContent = dataToChar.join('');
 
 
+    //DECRYPTION
 
-    //DECRIPTION
-    console.log(enc);
-
+    /**
+     * function decrypt(){
+     * 
+     * }
+     */
 }
